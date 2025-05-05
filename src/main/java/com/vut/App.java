d2bdaa93f23a43b9771815c1ae689de6c81ac352
Mr. Matsela <41293744+malefane1@users.noreply.github.com>
2025-05-05 15:03:38 +0200

    Update App.java

--- src/main/java/com/vut/App.java
+++ src/main/java/com/vut/App.java
@@ -5,7 +5,7 @@ package com.vut;
  *
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Simple Java Calculator!");
+    public String getGreetings() {
+        System.out.println("Greetings to you all!");
     }
 }
