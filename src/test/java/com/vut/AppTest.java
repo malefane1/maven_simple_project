95362c93125632eb2306654e5e85798f66a87dc5
Mr. Matsela <41293744+malefane1@users.noreply.github.com>
2025-05-05 14:59:00 +0200

    Update AppTest.java

--- src/test/java/com/vut/AppTest.java
+++ src/test/java/com/vut/AppTest.java
@@ -15,6 +15,6 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+        assertTrue( new App().main().equals("Simple Java Calculator!");
     }
 }
